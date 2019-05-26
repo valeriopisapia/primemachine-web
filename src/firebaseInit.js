32a27508b7a3811053fe6c723546be4827281962
Valerio <pisa.vale@gmail.com>
2019-05-26 15:44:04 +0200

    Update firebaseInit.js

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -6,30 +6,30 @@ const host = window.location.host.split('.')[0]
 let firebaseMainConfig = null
 
 const firebaseConfigDevelop = {
-  apiKey: 'AIzaSyC7ZqtM7KmJIGG_9mKyr8NJLEM3SoL04RU',
-  authDomain: 'stanwood-crm-dev.firebaseapp.com',
-  databaseURL: 'https://stanwood-crm-dev.firebaseio.com',
-  projectId: 'stanwood-crm-dev',
-  storageBucket: 'stanwood-crm-dev.appspot.com',
-  messagingSenderId: '879156665448'
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: ''
 }
 
 const firebaseConfigStage = {
-  apiKey: 'AIzaSyC7ZqtM7KmJIGG_9mKyr8NJLEM3SoL04RU',
-  authDomain: 'stanwood-crm-dev.firebaseapp.com',
-  databaseURL: 'https://stanwood-crm-dev.firebaseio.com',
-  projectId: 'stanwood-crm-dev',
-  storageBucket: 'stanwood-crm-dev.appspot.com',
-  messagingSenderId: '879156665448'
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: ''
 }
 
 const firebaseConfigProduction = {
-  apiKey: 'AIzaSyDZQFMyoH7FAs1-qMWyVTHVpYBB5vNmxts',
-  authDomain: 'stanwood-crm.firebaseapp.com',
-  databaseURL: 'https://stanwood-crm.firebaseio.com',
-  projectId: 'stanwood-crm',
-  storageBucket: 'stanwood-crm.appspot.com',
-  messagingSenderId: '991468360642'
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: ''
 }
 
 if (
